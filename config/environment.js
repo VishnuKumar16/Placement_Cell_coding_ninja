c11fe91f05a679ed327d5c69c86e47df7750c689
VishnuKumar16 <138098422+VishnuKumar16@users.noreply.github.com>
2023-11-04 19:32:33 +0530

    first commit

---
+++ config/environment.js
@@ -0,0 +1,11 @@
+const developement = {
+    name :'developement',
+    db_path :'./config/mongoose',
+    passport_path : './config/passport-local-strategy',
+    customMware_path : './config/flashMessage',
+    assets_path : './assets',
+    secret_key :'#placementCell@careercamp$$Team&&&interface##Amit^^Singh%%',
+    mongoose_path :'mongodb+srv://vis:vis1234@cluster0.sz2lfjp.mongodb.net/?retryWrites=true&w=majority',
+    api_path :'https://remotive.com/api/remote-jobs'
+}
+module.exports = developement;
